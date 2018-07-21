78237e74d83e963fa87f15f143104cce2f740116
tunime <almaseb_123@hotmail.com>
2018-07-21 18:43:20 -0500

    subiendo las presentacion

--- app/src/main/java/com/example/prueba/quehay/Main2Activity.java
+++ app/src/main/java/com/example/prueba/quehay/Main2Activity.java
@@ -2,6 +2,7 @@ package com.example.prueba.quehay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public void restaurante(View view){
+
+    }
+    public void dicoteca(View view){
+
+    }
+    public void karaoke(View view){
+
+    }
+    public void cine(View view){
+
+    }
+    public void cafeteria(View view){
+
     }
 }
